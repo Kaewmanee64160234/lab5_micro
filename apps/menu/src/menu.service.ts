00558a1b98e16535ae8a93c74307993ea85ecc6f
Kaewmanee <64160234@go.buu.ac.th>
2024-08-02 20:52:08 +0700

    chore: Update app.listen port to 3001

--- apps/menu/src/menu.service.ts
+++ apps/menu/src/menu.service.ts
@@ -2,7 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MenuService {
+  private menu = [
+    { id: 1, name: 'Burger', price: 10 },
+    { id: 2, name: 'Pizza', price: 15 },
+    { id: 3, name: 'Pasta', price: 8 },
+  ];
   getHello(): string {
     return 'Hello World!';
+  }
+  // get meeu
+  getMenu() {
+    return this.menu;
+  }
+
+  // get manu by id
+  async getMenuById(id: number) {
+    const item = await this.menu.find((menu) => menu.id === id);
+    if (!item) {
+      throw new Error('Item not found');
+    }
+    return item;
   }
 }
