1bf1743a21aa185662186ede85d9d816efdd60ea
Kaewmanee <64160234@go.buu.ac.th>
2024-08-02 20:59:42 +0700

    chore: Update API Gateway controller to use HttpService for menu endpoints

--- apps/api-gateway/src/api-gateway.controller.ts
+++ apps/api-gateway/src/api-gateway.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiGatewayService } from './api-gateway.service';
+import { Controller, Get, HttpServer } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Controller()
 export class ApiGatewayController {
-  constructor(private readonly apiGatewayService: ApiGatewayService) {}
+  constructor(private httpService: HttpService) {}
 
-  @Get()
-  getHello(): string {
-    return this.apiGatewayService.getHello();
+  @Get('menu')
+  async getMenu() {
+    const res = await firstValueFrom(
+      this.httpService.get('http://localhost:3001/menu'),
+    );
+
+    return res.data;
+  }
+
+  // get menu by id
+  @Get('menu/:id')
+  async getMenuById(id: number) {
+    const res = await firstValueFrom(
+      this.httpService.get(`http://localhost:3001/menu/${id}`),
+    );
+
+    return res.data;
   }
 }
